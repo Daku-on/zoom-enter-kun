f8dd5b9ae31ddc27552fcb4d888254c5d16bd6d3
Daku-on <takako.recruit@gmail.com>
2022-09-15 09:49:56 +0900

    今朝の進捗

--- zoom-enter-kun.py
+++ zoom-enter-kun.py
@@ -10,16 +10,19 @@ def ask_schedule():
     tk.Tk().withdraw()
     time_to_zoom = simpledialog.askstring('時刻入力', '何時にzoomに入りますか (HH:MM)')
     # dt = datetime.strptime("2018/06/05 13:45:06", "%Y/%m/%d %H:%M:%S") これに変更 http://hxn.blog.jp/archives/9800548.html
+    tday = datetime.date.today().strftime('%Y%m%d')                                        
+    
     try:
-        scheduled_time = datetime.strptime(ask_schedule.time_to_zoom, '%H:%M')
+        scheduled_time = datetime.datetime.strptime(tday + time_to_zoom, '%Y%m%d%H:%M')
+        return scheduled_time
     except:
         print('形式が正しくありません。')
-    return time_to_zoom
+        return sys.exit()
 
 def enter_zoom():
     webbrowser.open(url, new=0, autoraise=True)
     
-def done_message(delay_sec):
+def done_message(delay_sec): #この関数バグってそう
     now = datetime.datetime.now()
     scheduled_time = (now + datetime.timedelta(seconds = delay_sec)).isoformat(' ')
     top = tk.Toplevel()
@@ -42,25 +45,24 @@ def current_dir():
 
     # cf. https://qiita.com/typeling1578/items/6e86cfc1febb2cf53141
 
-def make_delay(scheduled_time):
+def make_delay(what_time):
     #ここらへんintとdatetime型でごっちゃってる
     min_delay_sec = 120
     max_delay_sec = 300
     ran_seconds = random.randint(min_delay_sec,max_delay_sec) # 3分〜5分前の間でランダムに入室時刻をずらす。5分前以降に起動した場合の例外処理が必要
     now = datetime.datetime.now()
-    delay = (scheduled_time - now).total_seconds()
+    delay = (what_time - now).total_seconds()
     if delay > max_delay_sec:
-        delay = (scheduled_time - now).total_seconds() - ran_seconds
+        delay = (what_time - now).total_seconds() - ran_seconds
     else: 
         delay = 5
     return delay
 # ---------------------------------------------    
-file = open(current_dir.path,'r')
+file = open(current_dir(),'r')
 url = file.read()
 file.close()
 
-ask_schedule()
-make_delay(ask_schedule.time_to_zoom)
-threading.Timer(make_delay.delay, enter_zoom).start()
-done_message(make_delay.delay)
+what_time = ask_schedule()
+threading.Timer(make_delay(what_time), enter_zoom).start()
+#done_message(make_delay(what_time))
 # https://stackoverflow.com/questions/30235587/closing-tkmessagebox-after-some-time-in-python?rq=1
